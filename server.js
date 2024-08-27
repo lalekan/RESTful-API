6abd5f579b697a7c229935338d550953551cf432
lalekan <lekan.suleiman@gmail.com>
2024-08-26 20:24:04 -0700

    added calculator

---
+++ server.js
@@ -0,0 +1,35 @@
+// Import Dependencies
+const express = require("express")
+
+const app = express()
+
+const PORT = 3000
+let total = 0
+
+// Get route
+app.get('/calculator', (req, res) => {
+    res.status(200).json({total})
+})
+
+app.use(express.json())
+
+app.post('/calculator', (req, res) => {
+    const number  = req.body.number
+    const operation = req.body.operation
+
+    if (operation === 'add') {
+        total += number
+    } else if (operation === 'subtract') {
+        total -= number
+    } else if (operation === 'multiply') {
+        total *= number
+    } else if (operation === 'divide') {
+        total /= number
+    } else {
+        return res.json({ error: 'Invalid Operation'})
+    }
+    res.status(200).json({total})
+})
+
+// Listener
+app.listen(PORT, () => console.log(`Express is listening on port: ${PORT}`))
